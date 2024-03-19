c94ad415667bd614bfc472c0dcca285066709ee2
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-03-19 18:12:18 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,21 @@
 import "./globals.css";
 
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 import { Inter } from "next/font/google";
+// import { fetchLayout } from "./lib/functions";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  // const layout = await fetchLayout();
+  // console.log("LAYOUT", layout);
   return (
     <html lang="el">
       <body className={inter.className}>
+        <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
