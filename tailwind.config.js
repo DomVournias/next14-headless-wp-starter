7fb5429d2d50bc2eb7759ead00fcba748ff3291c
Dom Vournias <54661880+DomVournias@users.noreply.github.com>
2024-03-19 21:02:25 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+       colors: {
+        primary: "#",
+        accent: "#",
+        secondary: "#",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
